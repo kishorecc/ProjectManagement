94975f366830f02c9b378f4f2a74c0b4ea88ac54
CHINNASWAMY, KISHORE <kishorechinnaswamy@northwesternmutual.com>
2019-02-27 00:09:31 -0600

    Unit opencover nbench

---
+++ REST SERVICE/ProjManAPI.Tests/NBench.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using NBench.Util;
+using NBench;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjManAPI.Tests
+{
+    public class NBench
+    {
+        private Counter _counter;
+
+        [PerfSetup]
+        public void Setup(BenchmarkContext context)
+        {
+            _counter = context.GetCounter("TestCounter");
+        }
+
+        [PerfBenchmark(Description = "Test to ensure that a minimal throughput test can be rapidly executed.",
+            NumberOfIterations = 10, RunMode = RunMode.Throughput,
+            RunTimeMilliseconds = 10000, TestMode = TestMode.Test)]
+        [CounterThroughputAssertion("TestCounter", MustBe.GreaterThan, 10000000.0d)]
+        [MemoryAssertion(MemoryMetric.TotalBytesAllocated, MustBe.LessThanOrEqualTo, ByteConstants.ThirtyTwoKb)]
+        [GcTotalAssertion(GcMetric.TotalCollections, GcGeneration.Gen2, MustBe.ExactlyEqualTo, 0.0d)]
+        public void Benchmark()
+        {
+            _counter.Increment();
+        }
+
+        [PerfCleanup]
+        public void Cleanup()
+        {
+            // does nothing
+        }
+    }
+}
